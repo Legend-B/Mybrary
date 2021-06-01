8c6527010be6ca12047a355cd102a3792cb34c3b
Legend-B <blazestudio98@gmail.com>
2021-06-01 11:00:33 -0400

    Author Stuff

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
-const indexRouter = require('./routes/index')
 const mongoose = require('mongoose')
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bodyParser = require('body-parser')
 
 const app = express()
 
@@ -14,6 +16,7 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layouts')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
@@ -21,5 +24,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to the Mongoose!'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
